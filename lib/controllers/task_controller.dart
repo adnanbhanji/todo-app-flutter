d382eb36e76a529f0b6842ad1c0018a08b61697e
Adnan Bhanji <bhanjiadnan786@gmail.com>
2024-10-19 21:12:40 +0200

    Adding task controls

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,4 @@
+// TODO: Here we'll work on the logic for managing the tasks.
+// in the views, we already added the controller functions for the logic, 
+// but nothing works, so now we add the actual logic 
+// for managing tasks (add, complete, and get tasks)

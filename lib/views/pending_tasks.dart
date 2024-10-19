06e658217380000b724e3af1554281a9436d1fab
Adnan Bhanji <bhanjiadnan786@gmail.com>
2024-10-19 20:52:23 +0200

    Created view with list of tasks and button to add new tasks

---
+++ lib/views/pending_tasks.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/task.dart';
+
+class PendingTasksScreen extends StatelessWidget {
+  final List<Task> pendingTasks;
+
+  const PendingTasksScreen({Key? key, required this.pendingTasks})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pending Tasks')),
+      body: ListView.builder(
+        itemCount: pendingTasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(pendingTasks[index].title),
+            subtitle: Text(
+                'Due: ${pendingTasks[index].dueDate.toLocal().toString().split(' ')[0]}'),
+            trailing: Checkbox(
+              value: pendingTasks[index].isCompleted,
+              onChanged: (bool? value) {
+                // Handle task completion
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Open add task dialog
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}

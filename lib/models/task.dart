d3c2d2828311f7e93f83702dde04cefe4394a745
Adnan Bhanji <bhanjiadnan786@gmail.com>
2024-10-19 19:29:20 +0200

    Defining the task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,1 +1,13 @@
-// TODO : Have the main class that stores the data of the task 
+class Task {
+  String title;
+  String description;
+  DateTime dueDate;
+  bool isCompleted;
+
+  Task({
+    required this.title,
+    required this.description,
+    required this.dueDate,
+    this.isCompleted = false,
+  });
+}
